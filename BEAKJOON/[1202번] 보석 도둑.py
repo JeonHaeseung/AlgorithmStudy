ae32443ee7f1b4b05bed53e3997175b716ae893e
JeonHaeseung <414catherine@gmail.com>
2024-01-06 23:57:10 +0900

    solving: [1202번] 보석 도둑
    date: 2024.01.06
    version: v1

---
+++ BEAKJOON/[1202번] 보석 도둑.py
@@ -0,0 +1,15 @@
+"""
+문제 링크: https://www.acmicpc.net/problem/1202
+문제 이름: [1202] 보석 도둑
+카테고리: BEAKJOON
+최종 업로드 날짜: 2024.01.06
+버전: v1
+"""
+
+"""
+문제 풀이: 
+"""
+import sys
+
+# map 함수를 통해서 int 형으로 값을 받아옴
+N, K = map(int, sys.stdin.readline().split())
