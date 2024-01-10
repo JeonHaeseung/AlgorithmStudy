0fd97c792d2cb7b8b6f2d9940348fbcd2d43af53
JeonHaeseung <414catherine@gmail.com>
2024-01-11 00:58:38 +0900

    solved: [1202번] 보석 도둑
    date: 2024.01.11
    version: v1

--- BEAKJOON/[1202번] 보석 도둑.py
+++ BEAKJOON/[1202번] 보석 도둑.py
@@ -10,6 +10,7 @@
 문제 풀이: 
 """
 import sys
+import heapq
 
 # map 함수를 통해서 int 형으로 값을 받아옴
 N, K = map(int, sys.stdin.readline().split())
@@ -17,10 +18,21 @@ N, K = map(int, sys.stdin.readline().split())
 jew = []
 for _ in range(N):
     # 무게 M, 가격 V
-    jew.append(list(map(int, sys.stdin.readline.split())))
+    heapq.heappush(jew, list(map(int, sys.stdin.readline().split())))
 
 bags = []
 for _ in range(K):
     # 최대 무게 C 배열에 저장
     bags.append(int(sys.stdin.readline()))
 bags.sort()
+
+answer = 0
+tmp_jew = []
+for bag in bags:
+    while jew and bag >= jew[0][0]:
+        heapq.heappush(tmp_jew, -heapq.heappop(jew)[1])
+    if tmp_jew:
+        answer -= heapq.heappop(tmp_jew)
+    elif not jew:
+        break
+print(answer)
