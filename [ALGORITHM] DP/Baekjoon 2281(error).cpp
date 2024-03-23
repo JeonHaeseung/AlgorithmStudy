cb8c4d8aaec5f34e9cb987a0af3004089806b9d4
JeonHaeseung <414catherine@gmail.com>
2024-03-23 16:27:00 +0900

    error: [2281] 데스노트

---
+++ [ALGORITHM] DP/Baekjoon 2281(error).cpp
@@ -0,0 +1,78 @@
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int final_answer(int n, int m, vector<int> &name, vector<vector<int>> &dp) {
+    int answer = 0;
+    vector<int> visited(n, 0);
+    vector<int> space(m, m);
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            // 추가할 수 있는 가능성이 있음
+            if (!visited[j] && space[i] - name[j] >= 0) {
+
+                visited[j] = 1;
+                if (space[i] - name[j] > 0) {
+                    space[i] -= (name[j] + 1);
+                } else {
+                    space[i] -= name[j];
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            // 추가할 수 있는 가능성이 있음
+            if (!visited[j] && space[i] - name[j] >= 0) {
+
+                visited[j] = 1;
+                if (space[i] - name[j] > 0) {
+                    space[i] -= (name[j] + 1);
+                } else {
+                    space[i] -= name[j];
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        if (space[i] == 0) {
+            answer += 0;
+        }
+        if (space[i] > 0) {
+            answer += pow((space[i] + 1), 2);
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+
+    cin >> n >> m;
+
+    int row = m;
+    int col = m;
+
+    vector<int> name(n);
+    vector<vector<int>> dp(row, vector<int>(col, 0));
+
+    for (int i = 0; i < n; i++) {
+        cin >> name[i];
+    }
+
+    // O(nlogn) 시간 복잡도
+    sort(name.begin(), name.end(), greater<>()); // 내림차순
+
+    cout << final_answer(n, m, name, dp);
+}
