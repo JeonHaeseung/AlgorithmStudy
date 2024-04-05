6c9416775ded85e0aac579c4e5f7cfedfbf6e442
JeonHaeseung <414catherine@gmail.com>
2024-04-05 15:43:19 +0900

    solved: [1038번] 감소하는 수

--- [ALGORITHM] Brute Force/Baekjoon 1038.cpp
+++ [ALGORITHM] Brute Force/Baekjoon 1038.cpp
@@ -6,58 +6,43 @@
 버전: v1
 */
 
-/*문제 풀이: N은 1,000,000보다 작거나 같은 자연수 또는 0이라고 했지만,
-
+/*문제 풀이:  C(n, k) = n! / (k! * (n-k)!)로 순열을 사용하면 될 것이라고 생각했다.
  */
-
 #include <iostream>
-#include <queue>
 #include <vector>
 
 using namespace std;
 
-int factorial(int n) {
-    int result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
-
-int solution(int n) {
-    if (n >= 0 && n <= 9) { // 0~9까지라면 그대로 리턴
-        return n;
-    }
-
-    int left = n - 9;
-    // C(n, k) = n! / (k! * (n-k)!)
-    int digit = 2;
-    while (digit <= 10) { // 자릿수는 2번째 부터 시작
-        int number = factorial(9) / (factorial(digit) * factorial(9 - digit));
-        cout << number << " \n";
-        if (number < left) {
-            left -= number;
-            cout << "left " << left << " \n";
-        } else {
-            break;
-        }
-        digit++;
-    }
-
-    // 감소하는 숫자의 최대값인 9876543210 이상의 범위라면 return -1
-    if (digit > 10 && left > 0) {
+// 여기서 numbers는 long long 타입으로 선언해 놓고 정작 함수의 return 타입을 int로 해서 에러 발생
+long long solution(int n, vector<long long> &numbers) {
+    if (n > 1022)
         return -1;
+    if (n == 0)
+        return 0;
+
+    for (int i = 0; i <= 9; i++) {
+        numbers[i] = i;
     }
 
-    return 0;
+    int num = 10;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < (numbers[i] % 10); j++) {
+            numbers[num] = (numbers[i] * 10) + j;
+            num += 1;
+        }
+    }
+
+    return numbers[n];
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
     int n; // 감소하는 수 번호
     cin >> n;
-
-    cout << solution(n);
+    // 여기서 n+1로 백터 크기 설정하면 DoubleFree 에러 발생, 이유를 모르겠음
+    vector<long long> numbers(1000001, 0);
+    cout << solution(n, numbers);
 }
