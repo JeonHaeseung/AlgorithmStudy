c37d4d63410ea9a1f04c49fc8f2ca1071f2c86dc
JeonHaeseung <414catherine@gmail.com>
2024-05-16 22:19:39 +0900

    solved: [17071번] 숨바꼭질 5

--- [ALGORITHM] BFS/Baekjoon 17071.cpp
+++ [ALGORITHM] BFS/Baekjoon 17071.cpp
@@ -6,7 +6,10 @@
 버전: v1
 */
 
-/*접근 방식:
+/*접근 방식: 수빈이가 14초에 x라는 지점을 방문했고, 동생이 20초에 그 x지점을 방문한다면 둘은 만날 수 있다.
+왜냐하면 수빈이는 x-1, x+1을 계속 반복하면서 x지점에 짝수 번째마다 머물 수 있기 때문이다.
+같은 원리로 수빈이가 15초에 x라는 지점을 방문했고, 동생이 21초에 그 x지점을 방문한다면 둘은 만날 수 있다.
+이때 BFS에서 깊이를 조정하기 위해서 큐의 사이즈를 사용해준다.
  */
 
 #include <iostream>
@@ -16,6 +19,7 @@
 using namespace std;
 
 const int MAX = 500000;
+bool visited[2][MAX + 1]; // 수빈이가 방문하는 위치
 
 bool checkRange(int n) {
     if (n >= 0 && n <= MAX)
@@ -29,41 +33,46 @@ int solution(int n, int k) {
         return 0;
     }
 
-    vector<int> time(MAX + 1, -1);
     queue<int> q;
     int young = k; // 동생의 처음 위치
     int accel = 1; // 동생의 가속도
-    time[n] = 0;
     q.push(n);
 
     while (!q.empty()) {
         // 동생이 먼저 움직임
         young += accel;
-        accel++;
 
         // 동생이 움직인 결과가 범위 바깥이라면 종료
         if (!checkRange(young))
             return -1;
 
-        int curr = q.front();
-        q.pop();
+        if (visited[accel % 2][young]) {
+            return accel;
+        }
 
-        for (int nx : {curr + 1, curr - 1, curr * 2}) {
-            // 이미 방문했거나 범위 바깥이라면 계산하지 않음
-            if (time[nx] != -1 || !checkRange(nx)) {
-                continue;
-            } else {
-                time[nx] = time[curr] + 1;
-                q.push(nx);
+        // 큐에 들어있는 같은 깊이만큼의 노드를 한번에 사용하기
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            int curr = q.front();
+            q.pop();
 
-                cout << "수빈이: " << nx << " 동생: " << young << "\n";
-
-                // 수빈이와 동생이 움직인 결과의 위치가 같다면 종료
-                if (young == nx) {
-                    return time[nx];
+            for (int nx : {curr + 1, curr - 1, curr * 2}) {
+                if (nx == young) {
+                    return accel;
                 }
+                // 범위 바깥이라면 계산하지 않음
+                if (!checkRange(nx)) {
+                    continue;
+                }
+                if (visited[accel % 2][nx])
+                    continue;
+
+                visited[accel % 2][nx] = true;
+                q.push(nx);
             }
         }
+
+        accel++;
     }
 
     return -1;
